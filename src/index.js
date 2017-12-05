284cbd6cd8399e336e7f5c8e40712d3e118a6c1e
Amit Zur <sendwithchibo@gmail.com>
2017-12-05 09:09:56 +0200

    mobx example

--- src/index.js
+++ src/index.js
@@ -1,38 +1,98 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import page2 from './page2'
-import registerServiceWorker from './registerServiceWorker';
-import {
-    BrowserRouter as Router,
-    HashRouter,
-    Route,
-    NavLink
-} from 'react-router-dom';
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
+import jsonpP from 'jsonp-p';
 
-//import { Router } from 'react-router';
+let flag = true;
 
-import Stores from './stores'
-import { Provider } from 'mobx-react';
+const ProductService = {
+	DEFAULT_LINK: "http://api.walmartlabs.com/v1/search?query=bosch&format=json&categoryId=1072864_1067619_1231248_1067739&apiKey=rgzr9bdktsbm3d6qy8ppdevb&numItems=25&start=25",
+
+	async loadProducts(link = this.DEFAULT_LINK) {
+		return jsonpP(link).promise
+			.then(resp => {
+				if (flag) {
+					flag = false;
+					return new Promise((resolve) => {
+						setTimeout(() => {
+							resolve(resp);
+						}, 3000);
+					});
+				}
+
+				return resp;
+			})
+			.then(resp => {
+				resp.items.forEach(item => this.theFullData[item.itemId] = item);
+				return resp.items;
+			});
+	},
+
+	theFullData: {}
+};
+
+window.service = ProductService;
+
+const ProductStore = (id, link) => {
+	const store = observable({
+		id,
+		products: []
+	});
+
+	ProductService.loadProducts(link).then(products => store.products = products);
+
+	return store;
+};
+
+const AppStore = (...links) => {
+	const store = observable({
+		activeTab: 0,
+		// productListings: [new ProductStore(0, links[0])],
+		productListings: links.map((link, i) => new ProductStore(i, link)),
+		toggle() {
+			// if (store.productListings.length === 1) {
+			// 	store.productListings.push(new ProductStore(1, links[1]));
+			// }
+			store.activeTab = !store.activeTab;
+		}
+	});
+
+	return store;
+};
+
+const appStore = new AppStore(
+	"http://api.walmartlabs.com/v1/items?ids=222,369677640,12417832&apiKey=rgzr9bdktsbm3d6qy8ppdevb",
+	"http://api.walmartlabs.com/v1/items?ids=222,679562478,19336123,951908906,369677640&apiKey=rgzr9bdktsbm3d6qy8ppdevb"
+);
 
 
-ReactDOM.render(
-<Provider {...Stores}>
-<HashRouter>
-        <div>   
-      
-        
+const ProductListing = observer(({ store: { products, id }}) => {
+	return <div className="product-listing">
+		{ console.log("xxx render", id) }
+    <h2>List #{id+1}</h2>
+		{products.map(p => <div>
+			{ console.log("xxx render", p.itemId) }
+			{p.itemId}</div>)
+		}
+  </div>
+});
 
-        <Route exact path="/" component={App}/>
-        <Route path="/App" component={App}/>
-        <Route path="/page2" component={page2}/>
-            
-        
-        
-        </div>
-   </HashRouter>       
-</Provider>
+const App = observer(({ store: { productListings, activeTab, toggle }}) => (
+  <div className="app">
+    <div>{ productListings.length }</div>
+		{ console.log("xxx app render") }
+    <h1>Product Listings</h1>
+    <button onClick={toggle}>Toggle</button>
 
-, document.getElementById('root'));
-registerServiceWorker();
+		{/*{ productListings.map(store => <ProductListing store={store} />) }*/}
+
+		<ProductListing store={productListings[Number(activeTab)]} />
+		<hr />
+		<ProductListing store={productListings[0]} />
+  </div>
+));
+
+
+ReactDOM.render(<App store={appStore}/>, document.getElementById('root'));
